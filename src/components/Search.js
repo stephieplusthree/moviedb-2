bf81c259cb055f01d08d0217905b76701efd8c1e
stephanie <stephieplusthree@gmail.com>
2020-07-29 20:31:18 -0700

    searchbar added

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div>
+            <section className="searchbox-wrap">
+                <input type="text" placeholder="Search for a movie..." className="searchbox" />
+            </section>
+        </div>
+    );
+}
+
+export default Search;
